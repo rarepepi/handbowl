111a847ec60785b369fb9d8e074f4e70c38dd97e
Pepi <pepimartinez@protonmail.com>
2018-10-07 02:49:02 -0400

    new structure of handbowl layed out in comments

--- handbowl/core.py
+++ handbowl/core.py
@@ -9,7 +9,30 @@ from utils import logo
 def main():
 
     # Read the config file for settings and keys
-    # start the handshake class and the fis
+    # start the handshake api and the fishbowl api
+    # for handshake pass in the api key and server setting for fishbowl ? both from config file
+
+    # Check if handshake api key is valid and fishbowl connection is good
+    # if so then continue sync
+
+# if orders sync then customers and products have to sync no matter what
+    # if product sync is true
+    # then run product sync
+        # for every prodcut in fb check if hs has it if not then add it as a new one
+            # you can do that by checking if the url for that sku hs api call returns null or no product
+        # for every product in fishbowl get the current stock amount and pass that through a post request to hs
+            # make a hs method call update_inventory(sku, amount)
+
+    # if customer sync is true
+    # then run customer sync
+        # for every customer id in hs check if fb has it, if not then add it as a new customer
+            # fishbowl api: create_customer(dict)
+
+    # if order sync is true
+    # then run order sync
+        # check for the last 5 orders and if they are confimered then new_sales_order(dict)
+
+
 
     # Product Sync
     hs_products = hand.get_products()
--- readme.md
+++ readme.md
@@ -1,19 +1,8 @@
 # Handbowl
 
-Handbowl connects Handshake B2B Sales to Fishbowl Inventory by using the Handshake API and creating import spreadsheets for Fishbowl.
+Handbowl connects Handshake B2B Sales to Fishbowl Inventory by using both clients apis to sync the data. 
 
 ### Setup/Usage
 
 1. Clone the project into ~/Documents folder of user with fishbowl client
-
-2. Set Fishbowl Schedule module settings:
-
-    export(products) --> ~/Documents/products/fb_product.csv
-
-    export(inventory) --> ~/Documents/products/fb_inventory.csv
-
-    import(customers) --> ~/Documents/customers/hs_customers.csv
-
-    import(orders) --> ~/Documents/orders/SO_*.csv
-
-3. Set windows task to run handbowl/core.py every 10 mins
+2. Set windows task to run handbowl/core.py every 10 mins
